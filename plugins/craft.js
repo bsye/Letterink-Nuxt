d276bf4ad00548b2f02035d4ba667680d484893f
ShirkerZ <zubenkodevelop@gmail.com>
2022-05-27 10:04:05 +0200

    added singleWorks

--- plugins/craft.js
+++ plugins/craft.js
@@ -1,15 +1,15 @@
 //View https://wbrowar.com/article/code/craft-cms-live-preview-with-nuxt-js/
 
 export default function ({ $config, $graphql, query }) {
-  const headers = {}
+  const headers = {};
 
   if (query.token) {
-    headers['x-craft-token'] = query.token
+    headers["x-craft-token"] = query.token;
   }
 
   // Add GraphQL authorization token for non-public schemas
-  if ($config.craftAuthToken !== '') {
-    headers['Authorization'] = `Bearer ${$config.craftAuthToken}`
+  if ($config.craftAuthToken !== "") {
+    headers["Authorization"] = `Bearer ${$config.craftAuthToken}`;
   }
 
   $graphql.default.setHeaders(headers);
